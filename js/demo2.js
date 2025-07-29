a523cf662f5c23cb3a8c8e99218faea6f34fbb59
Tai Hardy <taiplus02082004@gmail.com>
2025-07-29 17:53:20 +0700

    btnhom

---
+++ js/demo2.js
@@ -0,0 +1,33 @@
+// // setTimeout(function(){
+// //     alert("ping....");
+// // },5000);//thuc hien sau 5000 miliseconds
+
+
+
+// // setInterval(function(){
+// //     alert("pong..,,");
+// // },300);
+
+
+
+// // var m= 10;
+// // var s = 0;
+// // var tg = setInterval(function(){
+// //     console.log(m+":"+s);
+// //     s--;
+// //     if(s<0){
+// //         m--;
+// //         s = 59;
+// //     }
+// //     if(m<0){
+// //         clearInterval(tg);
+// //     }
+// // },100);
+
+
+// console("task");
+// setTimeout(function(){
+//     alert("task 2");
+// },500);
+// alert("task 3");
+
